8aaedd44b5f4aa87e695f79bf5032f898d54ac18
knightsrv <shourav@e-solutionpark.com>
2014-11-19 18:51:48 +0600

    first commit

---
+++ config/initializers/assets.rb
@@ -0,0 +1,40 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+Rails.application.config.assets.precompile += %w( jquery-1.10.2.min.js )
+
+Rails.application.config.assets.precompile += %w( modernizr.js )
+Rails.application.config.assets.precompile += %w( jquery.sticky.js )
+Rails.application.config.assets.precompile += %w( jquery.fitvids.js )
+Rails.application.config.assets.precompile += %w( jquery.easing-1.3.pack.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap-modal.js )
+Rails.application.config.assets.precompile += %w( jquery.parallax-1.1.3.js )
+Rails.application.config.assets.precompile += %w( jquery-countTo.js )
+Rails.application.config.assets.precompile += %w( jquery.appear.js )
+Rails.application.config.assets.precompile += %w( jquery.easy-pie-chart.js )
+Rails.application.config.assets.precompile += %w( jquery.cycle.all.js )
+Rails.application.config.assets.precompile += %w( jquery.maximage.js )
+Rails.application.config.assets.precompile += %w( jquery.isotope.min.js )
+Rails.application.config.assets.precompile += %w( smoothscroll.js )
+Rails.application.config.assets.precompile += %w( skrollr.js )
+Rails.application.config.assets.precompile += %w( jquery.flexslider-min.js )
+Rails.application.config.assets.precompile += %w( jquery.hoverdir.js )
+Rails.application.config.assets.precompile += %w( jquery.validate.min.js )
+Rails.application.config.assets.precompile += %w( portfolio_custom.js )
+Rails.application.config.assets.precompile += %w( script.js )
+Rails.application.config.assets.precompile += %w( retina-1.1.0.min.js )
+Rails.application.config.assets.precompile += %w( google-map.js )
+
+Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( normalize.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( flexslider.css )
+Rails.application.config.assets.precompile += %w( style-responsive.css )
+Rails.application.config.assets.precompile += %w( isotope.css )
+Rails.application.config.assets.precompile += %w( blu.css )
